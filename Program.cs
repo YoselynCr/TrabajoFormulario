b127a7bcb128d0d7bc93243bdd1c743bb6331867
ricardo-alexis <ricardoav0907@gmail.com>
2020-09-24 20:43:24 -0500

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -6,9 +6,37 @@ namespace Formulario_Basico
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("Bienvenidos a La POO");
-            Console.WriteLine("Hola");
+            string n, t, o, c;
+
+            Console.Write("Por favor ingresese su nombre: ");
+            n = Console.ReadLine();
+            Console.WriteLine(n);
+
+            Console.Write("Telefono: ");
+            t = Console.ReadLine();
+            Console.WriteLine(t);
+
+            Console.Write("Ocupación: ");
+            o = Console.ReadLine();
+            Console.WriteLine(o);
+
+            Console.Write("Correo electronico: ");
+            c = Console.ReadLine();
+            Console.WriteLine(c);
+            int e;
+
+            Console.Write("Edad: ");
+            e = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(e);
+            if (e <=18)
+            {
+                Console.WriteLine("Error en el registro, menor de edad.");
+            }
+            else
+            {
+                Console.WriteLine("Registro exitoso.");
+            }
+            Console.ReadLine()
         }
     }
 }
